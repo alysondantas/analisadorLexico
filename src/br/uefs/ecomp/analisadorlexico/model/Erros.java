6f5ae23574ec892a893512b1b7698d832f71f80d
Alyson Dantas <alyson.lipe@gmail.com>
2018-09-13 14:11:03 -0300

    Id de erros

--- src/br/uefs/ecomp/analisadorlexico/model/Erros.java
+++ src/br/uefs/ecomp/analisadorlexico/model/Erros.java
@@ -10,9 +10,19 @@ package br.uefs.ecomp.analisadorlexico.model;
  * @author alyso
  */
 public abstract class Erros {
-        public abstract class IdComentario{
-            public static final int Aspas = 0;
+        public abstract class Id{
+            public static final int ComentarioFinalAberto = 1;
+            public static final int ComentarioInicioAberto = 2;
+            public static final int CadeiaCharsInicioAberto = 3;
+            public static final int CadeiaCharsFinalAberto = 4;
+            
+        }
         
+        public abstract class Valor{
+            public static final String ComentarioFinalAberto = "Final de delimitador de comentario não fechado";
+            public static final String ComentarioInicioAberto = "Inicio de delimitador de comentario não fechado";
+            public static final String CadeiaCharsInicioAberto = "Inicio de cadeia de caracteres sem aspas";
+            public static final String CadeiaCharsFinalAberto = "Final de cadeia de caracteres sem aspas";
         }
     
 }
