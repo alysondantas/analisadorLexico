f61c963f9eb938556d96b0f15668b762f56150da
Alyson Dantas <alyson.lipe@gmail.com>
2018-09-13 14:24:31 -0300

    Erros de simbolo e indentificador

--- src/br/uefs/ecomp/analisadorlexico/model/Erros.java
+++ src/br/uefs/ecomp/analisadorlexico/model/Erros.java
@@ -15,14 +15,18 @@ public abstract class Erros {
             public static final int ComentarioInicioAberto = 2;
             public static final int CadeiaCharsInicioAberto = 3;
             public static final int CadeiaCharsFinalAberto = 4;
+            public static final int SimboloInvalido = 5;
+            public static final int IdentificadorInvalido = 6;
             
         }
         
         public abstract class Valor{
-            public static final String ComentarioFinalAberto = "Final de delimitador de comentario não fechado";
-            public static final String ComentarioInicioAberto = "Inicio de delimitador de comentario não fechado";
-            public static final String CadeiaCharsInicioAberto = "Inicio de cadeia de caracteres sem aspas";
-            public static final String CadeiaCharsFinalAberto = "Final de cadeia de caracteres sem aspas";
+            public static final String ComentarioFinalAberto = "ERRO: Final de delimitador de comentario não fechado.\n";
+            public static final String ComentarioInicioAberto = "ERRO: Inicio de delimitador de comentario não fechado.\n";
+            public static final String CadeiaCharsInicioAberto = "ERRO: Inicio de cadeia de caracteres sem aspas.\n";
+            public static final String CadeiaCharsFinalAberto = "ERRO: Final de cadeia de caracteres sem aspas.\n";
+            public static final String SimboloInvalido = "ERRO: Simbolo invalido encontrado.\n";
+            public static final String IdentificadorInvalido = "ERRO: Digito seguido de letra encontrado.\n";
         }
     
 }
