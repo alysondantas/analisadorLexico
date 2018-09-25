1a6752a71e00245c8918511ec73bb606af1410f5
Alyson Dantas <alyson.lipe@gmail.com>
2018-09-24 23:02:23 -0300

    analisador de comentarios

--- src/br/uefs/ecomp/analisadorlexico/controller/ControllerDados.java
+++ src/br/uefs/ecomp/analisadorlexico/controller/ControllerDados.java
@@ -16,6 +16,7 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.regex.Pattern;
 
 /**
@@ -23,79 +24,148 @@ import java.util.regex.Pattern;
  * @author Alyson Dantas
  */
 public class ControllerDados {
-    
+
     private static ControllerDados unicaInstancia;
     private String conteudoArq = ""; //String para salvar o conteúdo do arquivo original antes da compactação
     private String caminhoArq = "";
+    private int op = 0;
+    private int contLinha = 0;
+    private int auxI = 0;
     private ArrayList contedudoArqLista;
+    private char[] caracteres;
+
     File diretorio;
-    
 
-	private ControllerDados() {
-            contedudoArqLista = new ArrayList<String>();
-            diretorio = new File(caminhoArq);
-	}
+    private ControllerDados() {
+        contedudoArqLista = new ArrayList<String>();
+        diretorio = new File(caminhoArq);
+    }
 
-	/**
-	 * controla o instanciamento de objetos Controller
-	 *
-	 * @return unicaInstancia
-	 */
-	public static synchronized ControllerDados getInstance() {
-		if (unicaInstancia == null) {
-			unicaInstancia = new ControllerDados();
-		}
-		return unicaInstancia;
-	}
+    /**
+     * controla o instanciamento de objetos Controller
+     *
+     * @return unicaInstancia
+     */
+    public static synchronized ControllerDados getInstance() {
+        if (unicaInstancia == null) {
+            unicaInstancia = new ControllerDados();
+        }
+        return unicaInstancia;
+    }
 
-	/**
-	 * reseta o objeto Controller ja instanciado
-	 */
-	public static void zerarSingleton() {
-		unicaInstancia = null;
-	}
-        
-        public String lerArquivo(String local) throws IOException { //método que lê um arquivo de texto e converte todo o seu conteúdo para uma String
-		Reader arq = new InputStreamReader(new FileInputStream(local), "ISO-8859-1"); //inicializo arq como a leitura de arquivos no local especificado no padrão ISO-8859-1
-		BufferedReader buffRead = new BufferedReader(arq); //crio um novo objeto BuffReader e passo para ele a leitura do local em arq
-		String linha = buffRead.readLine(); //crio uma string auxiliar e já armazeno a primeira linha do arquivo
-		String c = ""; //crio uma string auxiliar para armazenar o conteúdo da string
-		
-		while(linha != null) { //enquanto não chegar no fim do arquivo
-			c = c + linha; //salvo o a letra da string na posição atual
-			linha = buffRead.readLine(); //salvo a linha atual do arquivo na string
-			if(linha!=null){
-                                contedudoArqLista.add(linha);
-				c = c + "\n"; //acrescento uma quebra de linha em "c" a cada fim de linha em "linha"
-                        }
-		}
-		buffRead.close(); //fecho a leitura do arquivo
+    /**
+     * reseta o objeto Controller ja instanciado
+     */
+    public static void zerarSingleton() {
+        unicaInstancia = null;
+    }
 
-		conteudoArq = c;//Uma varaivel local recebe a string com todo o conteudo do arquivo
-		return c; //retorna a String com todo o conteúdo do arquivo de texto
-	}
+    public String lerArquivo(String local) throws IOException { //método que lê um arquivo de texto e converte todo o seu conteúdo para uma String
+        Reader arq = new InputStreamReader(new FileInputStream(local), "ISO-8859-1"); //inicializo arq como a leitura de arquivos no local especificado no padrão ISO-8859-1
+        BufferedReader buffRead = new BufferedReader(arq); //crio um novo objeto BuffReader e passo para ele a leitura do local em arq
+        String linha = buffRead.readLine(); //crio uma string auxiliar e já armazeno a primeira linha do arquivo
+        String c = ""; //crio uma string auxiliar para armazenar o conteúdo da string
 
-	public void escreverArquivo(String texto, String local, String nome) throws IOException { //método deixado aqui só para inspiração. Não vai ser usado do jeito que está descrito no momento
-		local = local + nome; //anexo o nome do arquivo ao local que ele será escrito
-		BufferedWriter buffWrite = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(local), "ISO-8859-1")); //crio um novo objeto para escrita de arquivos e passo como parâmetro um novo objeto de escrita do arquivo no local especificado no padrão ISO-8859-1
-		buffWrite.append(texto); //anexo essa string no arquivo de texto
-		buffWrite.close(); //fecho o arquivo aberto
-	}
-        
-        public void listaArquivos(){
-            File arquivos[];
-            arquivos = diretorio.listFiles();
-            for(int i = 0; i < arquivos.length; i++){
-                //leia arquivos[i];
+        while (linha != null) { //enquanto não chegar no fim do arquivo
+            c = c + linha; //salvo o a letra da string na posição atual
+            linha = buffRead.readLine(); //salvo a linha atual do arquivo na string
+            if (linha != null) {
+                contedudoArqLista.add(linha);
+                c = c + "\n"; //acrescento uma quebra de linha em "c" a cada fim de linha em "linha"
             }
         }
-        
-        public void setConteudoArquivo(String arqmod){
-		conteudoArq = arqmod;
-	}
-        
-        public String getConteudoArquivo(){
-		return conteudoArq;
-	}
-    
+        buffRead.close(); //fecho a leitura do arquivo
+
+        conteudoArq = c;//Uma varaivel local recebe a string com todo o conteudo do arquivo
+        return c; //retorna a String com todo o conteúdo do arquivo de texto
+    }
+
+    public void escreverArquivo(String texto, String local, String nome) throws IOException { //método deixado aqui só para inspiração. Não vai ser usado do jeito que está descrito no momento
+        local = local + nome; //anexo o nome do arquivo ao local que ele será escrito
+        BufferedWriter buffWrite = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(local), "ISO-8859-1")); //crio um novo objeto para escrita de arquivos e passo como parâmetro um novo objeto de escrita do arquivo no local especificado no padrão ISO-8859-1
+        buffWrite.append(texto); //anexo essa string no arquivo de texto
+        buffWrite.close(); //fecho o arquivo aberto
+    }
+
+    public void listaArquivos() {
+        File arquivos[];
+        arquivos = diretorio.listFiles();
+        for (int i = 0; i < arquivos.length; i++) {
+            //leia arquivos[i];
+        }
+    }
+
+    public void analisadordelinhas() {
+        Iterator<String> itera = contedudoArqLista.iterator();
+        String aux = null;
+        auxI = 0;
+        while (itera.hasNext()) {
+            aux = itera.next();
+            if (aux != null) {
+                caracteres = aux.toCharArray();
+                for (auxI = 0; auxI < caracteres.length; auxI++) {
+                    //switch (op){
+                    //  case 0:
+                    if (caracteres[auxI] == '/') {
+                        if (auxI + 1 <= caracteres.length) {
+                            if (caracteres[auxI + 1] == '/') {
+                                //token comentario
+                                break;
+                            } else if (caracteres[auxI + 1] == '*') {
+                                itera = analisetokenComentario(itera);
+                            }
+                        } else {
+                            //erro
+                        }
+
+                        //analisetokenComentario(i,caracteres);
+                    }
+                    //    break;
+                    //}
+
+                    System.out.println("letra atual " + caracteres[auxI] + "\n");
+                }
+            }
+        }
+    }
+
+    public void setConteudoArquivo(String arqmod) {
+        conteudoArq = arqmod;
+    }
+
+    public String getConteudoArquivo() {
+        return conteudoArq;
+    }
+
+    private Iterator<String> analisetokenComentario(Iterator<String> itera) {
+        if (auxI < caracteres.length) {
+            int i = auxI + 1;
+            for (auxI = i; auxI < caracteres.length; auxI++) {
+                if (auxI + 1 <= caracteres.length) {
+                    if (caracteres[auxI] == '*' && caracteres[auxI + 1] == '/') {
+                        //escrevetoken
+                        return itera;
+                    }
+                }
+            }
+        } else {
+            contLinha++;
+            String aux = null;
+            while (itera.hasNext()) {
+                aux = itera.next();
+                caracteres = aux.toCharArray();
+                for (auxI = 0; auxI < caracteres.length; auxI++) {
+                    if (auxI + 1 <= caracteres.length) {
+                        if (caracteres[auxI] == '*' && caracteres[auxI + 1] == '/') {
+                            //escrevetoken
+                            return itera;
+                        }
+                    }
+                }
+            }
+        }
+        //escreveerro
+        return itera;
+    }
+
 }
--- src/br/uefs/ecomp/analisadorlexico/model/Analisador.java
+++ src/br/uefs/ecomp/analisadorlexico/model/Analisador.java
@@ -12,14 +12,71 @@ import java.util.regex.Pattern;
  */
 public class Analisador {
     private static final Pattern number = Pattern.compile("(-)?\\s*[0-9]+[0-9](.+[0-9]([0-9]))?");
+    private static final Pattern digit = Pattern.compile("[0-9]");
     private static final Pattern identific = Pattern.compile("[a-z]([a-z]|[0-9]|\\_)*");
+    private static final Pattern letter = Pattern.compile("[a-z][A-Z]");
+    private static final Pattern reserved = Pattern.compile("bool|class|constant|else|extends|false|float|if|int|main|method|read|return|string|then|true|variables|void|while|write");
+    private static final Pattern mathoperators = Pattern.compile("\\+|\\-|\\++|\\--|\\*|\\/");
+    private static final Pattern reloperators = Pattern.compile("\\!=|\\==|\\>|\\<|\\>=|\\<=|\\=");
+    private static final Pattern logoperators = Pattern.compile("\\!|\\&\\&|\\|\\|");
+    private static final Pattern delimiters = Pattern.compile("\\;|\\,|\\(\\)|\\[|\\]|\\{|\\}");
     
     public static boolean validarNumero(String palavra) {
         return number.matcher(palavra).matches();
     }
     
+    public static boolean validarDigito(String palavra) {
+        return digit.matcher(palavra).matches();
+    }
+    
     public static boolean validarIdentificador(String palavra) {
         return identific.matcher(palavra).matches();
     }
+    
+    public static boolean validarPalavrasReservadas(String palavra) {
+        return reserved.matcher(palavra).matches();
+    }
+    
+    public static boolean validarLetra(String palavra) {
+        return letter.matcher(palavra).matches();
+    }
+    
+    public static boolean validarOperadoresAritimeticos(String palavra) {
+        return mathoperators.matcher(palavra).matches();
+    }
+    
+    public static boolean validarOperadoresRelacionais(String palavra) {
+        return reloperators.matcher(palavra).matches();
+    }
+    
+    public static boolean validarOperadoresLogicos(String palavra) {
+        return logoperators.matcher(palavra).matches();
+    }
+    
+    public static boolean validarDelimitadores(String palavra) {
+        return delimiters.matcher(palavra).matches();
+    }
 
+    public void verificador(String palavra){
+        if (validarPalavrasReservadas(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Palavra Reservada>\n");
+        }else if (validarIdentificador(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Identificador>\n");
+        }else if (validarNumero(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Número>\n");
+        }else if (validarDigito(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Digito>\n");
+        }else if (validarLetra(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Letra>\n");
+        }else if (validarOperadoresAritimeticos(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Operador Aritmético>\n");
+        }else if (validarOperadoresRelacionais(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Operador Relacional>\n");
+        }else if (validarOperadoresLogicos(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Operador Lógico>\n");
+        }else if (validarDelimitadores(palavra)) {
+            System.out.println("Token <"+palavra+"> Classificação <Delimitador>\n");
+        }
+                
+    }
 }
