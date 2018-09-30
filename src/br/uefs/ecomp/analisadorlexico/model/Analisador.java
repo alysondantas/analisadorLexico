8cdeb1a04a129292059f52932abfabbafe312825
Lucas Cardoso <thelucascardoso@gmail.com>
2018-09-30 12:54:08 -0300

    Regex numero feito

--- src/br/uefs/ecomp/analisadorlexico/model/Analisador.java
+++ src/br/uefs/ecomp/analisadorlexico/model/Analisador.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
  * @author Alyson Dantas e Fada Cardoso
  */
 public class Analisador {
-    private static final Pattern number = Pattern.compile("(-)?\\s*[0-9]+[0-9](.[0-9]([0-9])*)?");
+    private static final Pattern number = Pattern.compile("(-)?\\s*[0-9][0-9]*(.[0-9]([0-9])*)?");
     private static final Pattern digit = Pattern.compile("[0-9]");
     private static final Pattern identific = Pattern.compile("[a-zA-Z]([a-zA-Z]|[0-9]|\\_)*");
     private static final Pattern letter = Pattern.compile("[a-zA-Z]");
